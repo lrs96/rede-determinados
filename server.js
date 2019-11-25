95488e298c08f8ded9d117b4d2870fa387f7ab44
Luan Rodrigues <internet29@kalunga.com.br>
2019-11-25 11:12:33 -0300

    Upload do projeto

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true,
+}))
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render("index.ejs")
+});
+
+const PORT = 3030;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Aperte CTRL + C para derrubar o servidor`)
+})
